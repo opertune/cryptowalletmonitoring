32cd82e4a04b2a30d04d5c51c50ee9b45d9cea3e
Romain <r.baucheron@gmail.com>
2022-07-28 22:08:12 +0200

    Timestamp update

--- src/Service/Exchange/Binance.php
+++ src/Service/Exchange/Binance.php
@@ -26,7 +26,7 @@ class Binance
          * SecretKey is key and params is value for hmac operation.
          */
 
-        $timestamp = round(microtime(true) * 1000) - 3000; // -3000
+        $timestamp = round(microtime(true) * 1000); // -3000
         $recvWindow = 60000;
         $params = http_build_query([
             'timestamp' => $timestamp,
